c2c2dcf09db8201a09926ca3e0de92850e298aba
maxwelb-assis <max.assis@ymail.com>
2020-11-04 23:04:11 -0300

    iniciando projeto

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import * as S from "./styles";
+
+function Home() {
+  return (
+    <>
+      <S.Container></S.Container>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import bgimg from "./bg1.jpg";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: url(${bgimg}) no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
