96f0aca1581dbfdd83d6d597440d8bf97640b5d8
maxwelb-assis <max.assis@ymail.com>
2020-11-05 01:32:32 -0300

    colocando a logo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,27 @@
 import React from "react";
-
 import * as S from "./styles";
+import logowhite from "../../components/assets/logo-white.jpg";
 
 function Home() {
   return (
     <>
-      <S.Container></S.Container>
+      <S.Container>
+        <S.Logo>
+          <S.Logowht src={logowhite} />
+        </S.Logo>
+        <S.ContainerCentral>
+          <div>
+            <S.Texto>
+              Descubra os melhores
+              <br /> restaurantes em sua cidade
+            </S.Texto>
+          </div>
+          <div>
+            <S.Input placeholder="Digite sua cidade"></S.Input>
+            <S.Button>BUSCAR</S.Button>
+          </div>
+        </S.ContainerCentral>
+      </S.Container>
     </>
   );
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import bgimg from "./bg1.jpg";
+import bgimg from "../../components/assets/bg1.jpg";
 
 export const Container = styled.div`
   width: 100%;
@@ -7,4 +7,48 @@ export const Container = styled.div`
   background: url(${bgimg}) no-repeat;
   background-size: cover;
   background-position: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Logo = styled.div`
+  margin-top: 70px;
+`;
+
+export const Logowht = styled.img``;
+
+export const ContainerCentral = styled.div`
+  margin-top: 100px;
+`;
+
+export const Texto = styled.h1`
+  color: #fff;
+  font-family: "Open Sans", sans-serif;
+  font-size: 3rem;
+  font-weight: 800;
+  line-height: 50px;
+`;
+
+export const Input = styled.input`
+  width: 40vw;
+  height: 6.5vh;
+  border: none;
+  margin-top: 35px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 300;
+  color: #767676;
+`;
+
+export const Button = styled.button`
+  color: #fff;
+  background-color: #39b54a;
+  border: none;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  width: 15vw;
+  height: 6.5vh;
+  margin-left: 10px;
+  margin-top: 35px;
 `;
